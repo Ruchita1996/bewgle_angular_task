bd54ef84f5f96aa1a6c902a42df809478146e61f
Ruchita1996 <chikhaleruchita@gmail.com>
2019-10-06 14:19:10 +0530

    add rating
    Signed-off-by: Ruchita1996 <chikhaleruchita@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JBL Go Portable Wireless Bluetooth Speaker with Mic (Black)';
+
+  @Input() rating: number;
+  @Input() itemId: number;
+  @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
+
+  inputName: string;
   
   constructor(private httpService: HttpClient)
   {}
   arrjbl:any [];
+  topScores: any [];
 
  
   
@@ -20,53 +27,24 @@ export class AppComponent {
 
   ngOnInit()
   {
+    
     this.httpService.get('./assets/data.json').subscribe(data => {
 
       this.arrjbl=[data];
+      this.topScores=data["topScores"]
       console.log(this.arrjbl.values);
+      
+
 
       
 
  },
+ 
     
       (err: HttpErrorResponse) =>{
         console.log(err.message);
-      }
+      })
     
-    )
-    
-  }
-  function() {
+}  
 
-    var myJSONObject = 
-      {
-      "topScores":
-          {
-              "prod_1": 
-              [
-                  {"topic" : "Sound Quality"},
-                  {"score" : "4.6"}
-              ],
-  
-              "prod_2": 
-              [
-                  {"topic" : "Bass"},
-                  {"score" : "4.2"}
-              ],
-              "prod_3": 
-              [
-                  {"topic" : "Design and Build"},
-                  {"score" : "4.2"}
-              ],
-              "prod_4": 
-              [
-                  {"topic" : "Battery Backup"},
-                  {"score" : "4.2"}
-              ]
-          }
-      }
-  
-   
-    }
-  
 }
